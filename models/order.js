fa7c44f65ff2a863873f05d38a3c6ea9b9e7effd
Aya-daabou <Ayadaaboul02@outlook.com>
2025-05-05 06:32:58 +0300

    fixed order history bug

--- models/order.js
+++ models/order.js
@@ -1,31 +1,51 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
-const Order = sequelize.define('Order', {
-  ID: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Order = sequelize.define(
+  "Order",
+  {
+    ID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    U_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "ID",
+      },
+    },
+    Status: {
+      type: DataTypes.ENUM(
+        "unconfirmed",
+        "confirmed",
+        "working on",
+        "to be delivered",
+        "delivered"
+      ),
+      defaultValue: "unconfirmed",
+    },
+    TotalAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      defaultValue: 0.0,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
   },
-  U_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Users',
-      key: 'ID'
-    }
-  },
-  Status: {
-    type: DataTypes.ENUM('unconfirmed', 'confirmed', 'working on', 'to be delivered', 'delivered'),
-    defaultValue: 'unconfirmed',
-  },
-  TotalAmount: {
-    type: DataTypes.DECIMAL(10, 2),
-    defaultValue: 0.00,
-  },
-}, {
-  timestamps: false,
-  tableName: 'Orders',
-});
+  {
+    tableName: "Orders",
+    timestamps: true,
+  }
+);
 
 module.exports = Order;
