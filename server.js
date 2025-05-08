a907c329d287164e2eefe880f3c0205f3eaa3944
Aya-daabou <Ayadaaboul02@outlook.com>
2025-05-08 18:59:55 +0300

    changed origin frontend domain

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(logger);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://change-later.com"],
+    origin: [
+      "http://localhost:5173",
+      "https://frontend-crochet-e-commerce.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
