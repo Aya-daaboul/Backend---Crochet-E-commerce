6e796e272cee33c912b68da43d4004f96e4db806
Aya-daabou <Ayadaaboul02@outlook.com>
2025-05-08 09:13:30 +0300

    missing parameter

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
 const authenticate = require("../middlewares/authMiddleware");
+const authorize = require("../middlewares/authorize");
 
 router.post("/add", authenticate, orderController.addToCart);
 router.get("/cart", authenticate, orderController.getCart);
